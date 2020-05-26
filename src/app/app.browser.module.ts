5a52234d7623042689e8417f4f77e56b239f9096
Thomas Handle <thomas.handle@gmail.com>
2020-05-27 00:00:30 +0200

    update

--- src/app/app.browser.module.ts
+++ src/app/app.browser.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserTransferStateModule } from '@angular/platform-browser';
-import { REQUEST } from '@nguniversal/express-engine/tokens';
+// import { REQUEST } from '@nguniversal/express-engine/tokens';
 import { AppComponent } from './app.component';
 import { AppModule } from './app.module';
 import { ServiceWorkerModule } from '@angular/service-worker';
@@ -8,9 +8,9 @@ import { environment } from '../environments/browser/environment';
 import { InlineStyleModule } from './inline-style/inline-style.module';
 import { InlineStyleComponent } from './inline-style/inline-style.component';
 
-export function getRequest(): any {
+/* export function getRequest(): any {
   return { headers: { cookie: document.cookie } };
-}
+} */
 
 @NgModule({
   bootstrap: [AppComponent, InlineStyleComponent],
@@ -20,12 +20,12 @@ export function getRequest(): any {
     InlineStyleModule,
     environment.production ? ServiceWorkerModule.register('./ngsw-worker.js', { enabled: environment.production }) : []
   ],
-  providers: [
+  /* providers: [
     {
       provide: REQUEST,
       useFactory: getRequest,
     },
     { provide: 'ORIGIN_URL', useValue: location.origin },
-  ],
+  ], */
 })
 export class AppBrowserModule {}
