3616dbc3a5bc823ca77791f72f1e8f5d24d8a945
Thomas Handle <thomas.handle@gmail.com>
2020-05-27 07:41:21 +0200

    update

--- src/app/app.browser.module.ts
+++ src/app/app.browser.module.ts
@@ -4,7 +4,7 @@ import { BrowserTransferStateModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { AppModule } from './app.module';
 import { ServiceWorkerModule } from '@angular/service-worker';
-import { environment } from '../environments/browser/environment';
+import { environment } from '../environments/environment';
 import { InlineStyleModule } from './inline-style/inline-style.module';
 import { InlineStyleComponent } from './inline-style/inline-style.component';
 
